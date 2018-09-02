31cea5ce835a4cfbd5bf357c40e23dd652c2d223
Ratan Srikanth <ratansrikanth@gmail.com>
2018-09-02 14:06:44 +0530

    Third Commit

---
+++ src/test/java/AirlinesDemo/TestBase.java
@@ -0,0 +1,15 @@
+package AirlinesDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+	}
+
+}
